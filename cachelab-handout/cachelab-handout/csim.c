94c4411f9e406209f3d25e1a7ad222a98c4fb11a
Junoth <heathmyh@gmail.com>
2020-11-14 13:18:45 -0500

    Finish Part A: Writing a Cache Simulator

--- cachelab-handout/cachelab-handout/csim.c
+++ cachelab-handout/cachelab-handout/csim.c
@@ -2,116 +2,174 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <limits.h>
+
+#define BIT(n) (1 << (n))
 
 struct cache_line {
   int valid_bit;
   int tag;
   int counter;
-}
+};
 
-struct node {
-  
-}
+struct cache_line** cache;
 
 int hit_count, miss_count, eviction_count;
 
-// Number of lines per set
-int num_of_line;
-
-// Numebr of block bits
-int num_of_block;
-
-// Number of set index bits
-int num_of_set;
-
-int verbose;
+int set_num, line_num, block_num, verbose;
 
 const char* sd;
 
-const char* HELP_INFO = "Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\nOptions:\n  -h         Print this help message.\n  -v         Optional verbose flag.\n  -s <num>   Number of set index bits.\n  -E <num>   Number of lines per set.\n  -b <num>   Number of block offset bits.\n  -t <file>  Trace file.\n\nExamples:\n  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n";
-
-// parse argument values
+/**
+ * parse the argument 
+ * 
+ * s: number of set
+ * E: number of line
+ * b: number of block
+ * v: verbose flag
+ * t: trace file
+ */
 void parse_argument(int argc, char *argv[]) {
 
-    char opt;
+  char opt;
     
-    while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
-        switch (opt) {
-            case 'h':
-                fprintf(stdout, "%s", HELP_INFO);
-                exit(0);
-            case 'v':
-                verbose = 1;
-                break;
-            case 's':
-                num_of_set = atoi(optarg);
-                break;
-            case 'E':
-                num_of_line = atoi(optarg);
-                break;
-            case 'b':
-                num_of_block = atoi(optarg);
-                break;
-            case 't':
-                sd = optarg;
-                break;
-            default:
-                fprintf(stderr, "Error: not defined argument");
-                exit(1);
-        }
-    }
-}
-
-
-
-// check argument input 
-void check_input() {
-    if (num_of_line <= 0 || num_of_set <= 0 || num_of_block <= 0) {
-        fprintf(stderr, "Error: not valid input\n");
+  while ((opt = getopt(argc, argv, "vs:E:b:t:")) != -1) {
+    switch (opt) {
+      case 'v':
+        verbose = 1;
+        break;
+      case 's':
+        set_num = atoi(optarg);
+        break; 
+      case 'E':
+        line_num = atoi(optarg);
+        break;
+      case 'b':
+        block_num = atoi(optarg);
+        break;
+      case 't':
+        sd = optarg;
+        break;
+      default:
+        fprintf(stderr, "Error: not defined argument");
         exit(1);
     }
+  }
 }
 
-void initialize_cache(cache_line* cache) {
-   check_input(); 
 
+/**
+ * Initialize the cache
+ */
+void initialize_cache() {
+  cache = (struct cache_line**)calloc(BIT(set_num+1), sizeof(struct cache_line*));
+  for (int i = 0; i < BIT(set_num+1); ++i) {
+    cache[i] = (struct cache_line*)calloc(line_num, sizeof(struct cache_line));
+  }
+}
 
+// evict the cache
+void lru(int tag, int set, int counter) {
+  int min_count = INT_MAX, index = 0, i;
+  for (i = 0; i < line_num; ++i) {
+    if (cache[set][i].valid_bit == 0) {
+      index = i;
+      break;
+    }
+
+    if (cache[set][i].counter < min_count) {
+      min_count = cache[set][i].counter;
+      index = i;
+    }
+  }
+
+  miss_count++;
+  if (cache[set][index].valid_bit == 1) {
+    eviction_count++;
+  }
+
+  cache[set][index].valid_bit = 1;
+  cache[set][index].tag = tag;
+  cache[set][index].counter = counter;
+}
+
+// count the hit/miss
+void count(int tag, int set, int counter) {
+  for (int i = 0; i < line_num; ++i) {
+    if (cache[set][i].valid_bit == 1 && cache[set][i].tag == tag) {
+      hit_count++;
+      cache[set][i].counter = counter;
+      return;
+    }
+  }
+
+  lru(tag, set, counter);
+}
+
+int get_set_mask() {
+  int mask = 0, i;
+  for (i = 0; i < set_num; ++i) {
+    mask = (mask << 1) | 1; 
+  } 
+  return mask;
+}
+
+int get_set(int address, int mask, int b) {
+  return (address >>= b) & mask;
+}
+
+int get_tag(int address, int s, int b) {
+  return address >> (b + s);
+}
+
+void free_memory() {
+  int sets = 2 << set_num;
+  for (int i = 0; i < sets; ++i) {
+    free(cache[i]);
+  }
 }
 
 // get trace from file
 void read_trace() {
-    FILE *fp;
-    if ((fp = fopen(sd, "r")) == NULL) {
-      fprintf(stderr, "Error: not valid file");
-      exit(1);
+  FILE *fp;
+  if ((fp = fopen(sd, "r")) == NULL) {
+    fprintf(stderr, "Error: not valid file");
+    exit(1);
+  }
+   
+  int mask = get_set_mask(), counter = 1;
+  char access_type;
+  unsigned long address;
+  int size;
+  while (fscanf(fp, " %c %lx,%d", &access_type, &address, &size) > 0) {
+    int tag = get_tag(address, set_num, block_num);
+    int set = get_set(address, mask, block_num);
+    switch(access_type) {
+      case 'L':
+        count(tag, set, counter++);
+        break;
+      case 'S':
+        count(tag, set, counter++);
+        break;
+      case 'M':
+        count(tag, set, counter++);
+        // store, no need to do for second time
+        hit_count++;
+        break;
+      default:
+        break;
     }
-    
-    char access_type;
-    unsigned long address;
-    int size;
-    while (fscanf(fp, " %c %lx,%d", &access_type, &address, &size) > 0) {
-      // todo
-    }
+  }
 
-    fclose(fp);
+  fclose(fp);
 }
-
-// 
-
 
 int main(int argc, char *argv[])
 {
-    parse_argument(argc, argv);
-
-    cache_line* cache;
-
-    initialize_cache(cache);
-
-    read_trace();
-
-    // test
-    printf("%d, %d, %d\n", num_of_set, num_of_line, num_of_block);
-       
-    printSummary(0, 0, 0);
-    return 0;
+  parse_argument(argc, argv);
+  initialize_cache();
+  read_trace();
+  free_memory(cache);
+  printSummary(hit_count, miss_count, eviction_count);
+  return 0;
 }
